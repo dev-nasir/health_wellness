336fac963d67f0c41cd0ccb5d5d7bbeeff97c58a
dev-nasir <nasirconceptrecall@gmail.com>
2025-07-26 15:09:02 +0500

    style button

--- src/components/ui/inputField.tsx
+++ src/components/ui/inputField.tsx
@@ -3,13 +3,13 @@ type Prop = { label: string; type: string; placeholder: string };
 const InputField = (props: Prop) => {
   return (
     <div>
-      <label className="block text-sm font-medium text-heading flex align-item-center">
+      <label className=" text-sm  text-heading flex align-item-center font-Chocolates font-light">
         {props.label}
       </label>
       <input
         type={props.type}
         name="name"
-        className="mt-2 w-full p-3 pb-4 border-b-1 border-b-text-heading focus:outline-none focus:ring-0 focus:ring-green-900"
+        className="mt-2 w-full p-3 pb-4 border-b-1 border-b-text-heading font-Chocolates font-light focus:outline-none focus:ring-0 focus:ring-green-900"
         placeholder={props.placeholder}
       />
     </div>
